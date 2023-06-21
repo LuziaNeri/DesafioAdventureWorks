bd44740cbb9bce25f7b5f575f7992ba15370fb30
Luzia Neri Correia Souza <luzia.souza@indicium.tech>
2023-06-21 08:30:37 -0300

    Atualizando dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,36 +1,30 @@
 with stg_customer as (
-    select 
-        id_cliente
-        , id_pessoa
-        , id_loja
+    select *
     from {{ref('stg_customer')}}
 )
 
 , stg_person as (
-    select
-        id_negocio
-        , nome_pessoa
+    select *
     from {{ref('stg_person')}}
 )
 
-, stg_store as (
+, join_tabelas as(
     select
-        id_negocio_loja
-        , nome_loja
-    from {{ref('stg_store')}}
+        stg_person.id_negocio
+        , stg_customer.id_cliente
+        , stg_customer.id_pessoa
+        , stg_customer.id_territorio
+        , stg_person.nome_pessoa
+    from stg_person
+    inner join stg_customer on stg_person.id_negocio = stg_customer.id_cliente
 )
 
-, transformacao as (
+, transformacao as(
     select
-    row_number() over (order by stg_customer.id_cliente) as customer_sk --surrogate key    
-    , stg_customer.id_cliente
-    , stg_person.id_negocio
-    , stg_person.nome_pessoa
-    , stg_store.id_negocio_loja
-    , stg_store.nome_loja
-    from stg_customer
-    inner join stg_person on stg_customer.id_pessoa = stg_person.id_negocio
-    inner join stg_store on stg_customer.id_loja = stg_store.id_negocio_loja
+        row_number() over (order by id_cliente) as customer_sk
+        , *  
+    from join_tabelas
 )
+
 select *
-from transformacao
+from join_tabelas
