414fb37d25f2346d701f6ef951e73cb63faf4250
Luzia Neri Correia Souza <luzia.souza@indicium.tech>
2023-07-06 15:23:36 -0300

    Atualização dodd README.md e requirements.txt, atualização das marts

--- tests/total_vendido_2011.sql
+++ tests/total_vendido_2011.sql
@@ -4,14 +4,14 @@
     )
 }}
 
----Esse teste soma as vendas 2011-05-31 e 2014-06-30. O resultado deve ser entre 110.373.889,30 e 110.373.889,32
+---Esse teste soma as vendas brutas entre 2011-01-31 e 2011-12-31. O valor total corresponde à $12.646.112,160699928
 with
     total_vendas_2011 as (
-        select sum (subtotal) as total_vendido
-        from {{ref('fct_order_details')}}
+        select sum (total_bruto) as total_vendido
+        from {{ref('fct_sales')}}
         where data_pedido between '2011-01-31' and '2011-12-31'
     )
 
 select total_vendido
 from total_vendas_2011
-where total_vendido not between 151706131.54 and 151706131.55
+where total_vendido not between 12646112.16 and 12646112.17
